0b6fa09c0af261e804aa436f24541ffbbc2e356d
Cam Linke <cam.linke@gmail.com>
2015-08-06 17:05:37 -0600

    added flask-migrate

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/rate')
+def rate():
+    return render_template('rate.html')
+
+@app.route('/recommendations')
+def recommendations():
+    return render_template('recommendations.html')

1237345b568af622d9a673fe6191c5367f9cbe36
Cam Linke <cam.linke@gmail.com>
2015-08-10 13:21:28 -0600

    fixed structre of data_importer

--- data_importer.py
+++ data_importer.py
@@ -1,45 +1,59 @@
 from app import db
 from app.models import Movie, Genre
+from flask.ext.script import Command
 import csv
 import os
 
-def movie_import():
-    with open("{}/ml-latest/movies.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "r") as movie_file:
-        movie_reader = csv.reader(movie_file, delimiter=',')
-        next(movie_reader, None) # Skip the header
-        for movie in movie_reader:
-            try:
-                movie_id = movie[0]
+class MovieImport(Command):
+    def run(self):
+        with open("{}/ml-latest/movies.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "r") as movie_file:
+            movie_reader = csv.reader(movie_file, delimiter=',')
+            next(movie_reader, None) # Skip the header
+            for movie in movie_reader:
+                try:
+                    movie_id = movie[0]
 
-                # Deal with movies that are enclosed in quotes
-                if movie[1].strip()[0] == '"':
-                    movie_name = movie[1].strip()[1:-8]
-                    movie_year = movie[1].strip()[-6:-2]
-                else:
-                    movie_name = movie[1].strip()[:-7]
-                    movie_year = int(movie[1].strip()[-5:-1])
+                    # Deal with movies that are enclosed in quotes
+                    if movie[1].strip()[0] == '"':
+                        movie_name = movie[1].strip()[1:-8]
+                        movie_year = movie[1].strip()[-6:-2]
+                    else:
+                        movie_name = movie[1].strip()[:-7]
+                        movie_year = int(movie[1].strip()[-5:-1])
 
-                genres = movie[2].split("|")
+                    genres = movie[2].split("|")
 
-                if Movie.query.filter_by(movie_id=movie_id).first():
-                    continue
-                movie = Movie(movie_id=movie_id, name=movie_name, year=movie_year)
-                db.session.add(movie)
+                    if Movie.query.filter_by(movie_id=movie_id).first():
+                        continue
+                    movie = Movie(movie_id=movie_id, name=movie_name, year=movie_year)
+                    db.session.add(movie)
 
-                if genres[0] != '(no genres listed)':
-                    for genre in genres:
-                        movie_genre = Genre(genre_name=genre, movie_id=movie_id)
-                        db.session.add(movie_genre)
+                    if genres[0] != '(no genres listed)':
+                        for genre in genres:
+                            movie_genre = Genre(genre_name=genre, movie_id=movie_id)
+                            db.session.add(movie_genre)
 
-                db.session.commit()
+                    db.session.commit()
 
-            except Exception as e:
-                print movie[1]
-                db.session.rollback()
-                print "error ", e
+                except Exception as e:
+                    print movie[1]
+                    db.session.rollback()
+                    print "error ", e
 
-        # I wrote this to find what movies didn't have dates so I could fix them by hand
-        # for movie in movie_reader:
-        #     if movie[1].strip()[-1] != ")":
-        #         print movie[1]
-        #         continue
+            # I wrote this to find what movies didn't have dates so I could fix them by hand
+            # for movie in movie_reader:
+            #     if movie[1].strip()[-1] != ")":
+            #         print movie[1]
+            #         continue
+
+class RatingsImport(Command):
+    def run(self):
+        with open("{}/ml-latest/ratings.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "r") as ratings_file:
+            ratings_reader = csv.reader(ratings_file, delimiter=',')
+            next(ratings_reader, None)
+            count = 0
+            for rating in ratings_reader:
+                count += 1
+                print rating
+                if count <= 10:
+                    break
--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 import os
-from data_importer import movie_import
+from data_importer import MovieImport, RatingsImport
 
 from app import app, db
 
@@ -12,7 +12,8 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
-manager.add_command('import_movies', movie_import())
+manager.add_command('import_movies', MovieImport)
+manager.add_command('import_ratings', RatingsImport)
 
 if __name__ == '__main__':
     manager.run()

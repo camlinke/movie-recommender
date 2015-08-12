5660ac6ea5acf3071dc530810929d694bf5f7813
Cam Linke <cam.linke@gmail.com>
2015-08-12 16:41:15 -0600

    overall ratings and cosine similarity code written

--- engine/engine.py
+++ engine/engine.py
@@ -1,5 +1,6 @@
 from pyspark import SparkContext
 from pyspark.mllib.recommendation import ALS
+from pyspark.mllib.linalg import Vectors
 import sys
 import os
 import csv
@@ -77,31 +78,84 @@ movieNameWithAvgRatingsRDD = (moviesRDD
                               .map(lambda x: (x[1][1][1], x[1][0], x[1][1][0])))
 # print movieNameWithAvgRatingsRDD.filter(lambda x: x[2] > 150).takeOrdered(3, lambda x: -x[0])
 
-trainingRDD, validationRDD, testRDD = ratingsRDD.randomSplit([6, 2, 2], seed=0L)
+# trainingRDD, validationRDD, testRDD = ratingsRDD.randomSplit([6, 2, 2], seed=0L)
 
-# ALS Prediction
-validationForPredictRDD = validationRDD.map(lambda x: (x[0], x[1]))
+# # ALS Prediction
+# validationForPredictRDD = validationRDD.map(lambda x: (x[0], x[1]))
 
-seed = 5L
-iterations = 5
-regularizationParameter = 0.1
-ranks = [4, 8, 12]
-errors = [0, 0, 0]
-err = 0
-tolerance = 0.02
+# seed = 5L
+# iterations = 5
+# regularizationParameter = 0.1
+# ranks = [4, 8, 12]
+# errors = [0, 0, 0]
+# err = 0
+# tolerance = 0.02
+# error = None
 
-minError = float('inf')
-bestRank = -1
-bestIteration = -1
+# minError = float('inf')
+# bestRank = -1
+# bestIteration = -1
 
-for rank in ranks:
-    model = ALS.train(trainingRDD, rank, seed=seed, iterations=iterations, lambda_=regularizedParameter)
-    predictedRatingsRDD = model.predictAll(validationForPredictRDD)
-    error = computeError(predictedRatingsRDD, validationRDD)
-    errors[err] = error
-    err += 1
-    print 'For rank %s the RMSE is %s' % (rank, error)
-    if error < minError:
-        minError = error
-        bestRank = rank
-print 'The best model was trained with rank %s' % bestRank
+# for rank in ranks:
+#     model = ALS.train(trainingRDD, rank, seed=seed, iterations=iterations, lambda_=regularizationParameter)
+#     predictedRatingsRDD = model.predictAll(validationForPredictRDD)
+#     error = compute_error(predictedRatingsRDD, validationRDD)
+#     errors[err] = error
+#     err += 1
+#     print 'For rank %s the RMSE is %s' % (rank, error)
+#     if error < minError:
+#         minError = error
+#         bestRank = rank
+
+# myModel = ALS.train(trainingRDD, bestRank, seed=seed, iterations=iterations,
+#                       lambda_=regularizationParameter)
+# testForPredictingRDD = testRDD.map(lambda x: (x[0], x[1]))
+# predictedTestRDD = myModel.predictAll(testForPredictingRDD)
+
+# testRMSE = compute_error(testRDD, predictedTestRDD)
+
+# print 'The model had a RMSE on the test set of %s' % testRMSE
+
+
+
+# Cosine Similarity for Individual Recommendations
+def dotprod(a, b):
+    return a.dot(b)
+
+def cosine_similarity(user1_ratings, user2_ratings):
+    """ Takes in two Dense Vectors of user's Recommendations and
+        returns the cosine similarity between the two.
+    """
+    return user1_ratings.dot(user2_ratings) / (math.sqrt(user1_ratings.dot(user1_ratings)) * math.sqrt(user2_ratings.dot(user2_ratings)))
+
+# Create sparse vectors grouped by usersId
+movies_length = moviesRDD.count()
+userIDsWithRatingsRDD = (ratingsRDD
+                         .map(lambda (user_id, movie_id, rating): (user_id, [(movie_id, rating)]))
+                         .reduceByKey(lambda a, b: a + b)
+                         .map(lambda x: (x[0], Vectors.sparse(movies_length, x[1]))))
+
+crossUsers = userIDsWithRatingsRDD.cartesian(userIDsWithRatingsRDD).filter(lambda x: x[0] != x[1]).cache()
+print crossUsers.take(1)
+similarities = crossUsers.map(lambda x: (x[0][0], x[1][0], cosine_similarity(x[0][1], x[1][1]))).cache()
+print "here"
+print "here"
+
+def similarities_for_user(user_id):
+    return similarities.filter(lambda x: x[0] == int(user_id)).takeOrdered(10, lambda x: -x[2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

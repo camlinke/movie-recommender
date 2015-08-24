e57035a21dfb638355fe35f6d8aab4d81551fda5
Cam Linke <cam.linke@gmail.com>
2015-08-23 18:03:03 -0600

    fixed some speed issues in recommendation engine

--- engine/engine.py
+++ engine/engine.py
@@ -116,52 +116,54 @@ else:
     raw_ratings = sc.textFile(small_ratings).repartition(num_partitions)
     raw_movies = sc.textFile(small_movies)
 
+# Collaborative Filtering
+
 def get_ratings_tuple(entry):
     items = entry.split(',')
     return int(items[0]), int(items[1]), float(items[2])
 
-def get_movie_tuple(entry):
-    items = entry.split(',')
-    return int(items[0]), items[1]
+# def get_movie_tuple(entry):
+#     items = entry.split(',')
+#     return int(items[0]), items[1]
 
-def get_counts_and_averages(IDandRatingsTuple):
-    id = IDandRatingsTuple[0]
-    length = len(IDandRatingsTuple[1])
-    average = 1.0 * sum(IDandRatingsTuple[1]) / length
-    return (id, (length, average))
+# def get_counts_and_averages(IDandRatingsTuple):
+#     id = IDandRatingsTuple[0]
+#     length = len(IDandRatingsTuple[1])
+#     average = 1.0 * sum(IDandRatingsTuple[1]) / length
+#     return (id, (length, average))
 
-def compute_error(predictedRDD, actualRDD):
-    # Transform predictedRDD into the tuples of the form ((UserID, MovieID), Rating)
-    predictedReformattedRDD = predictedRDD.map(lambda x: ((x[0], x[1]), x[2]))
+# def compute_error(predictedRDD, actualRDD):
+#     # Transform predictedRDD into the tuples of the form ((UserID, MovieID), Rating)
+#     predictedReformattedRDD = predictedRDD.map(lambda x: ((x[0], x[1]), x[2]))
 
-    # Transform actualRDD into the tuples of the form ((UserID, MovieID), Rating)
-    actualReformattedRDD = actualRDD.map(lambda x: ((x[0], x[1]), x[2]))
+#     # Transform actualRDD into the tuples of the form ((UserID, MovieID), Rating)
+#     actualReformattedRDD = actualRDD.map(lambda x: ((x[0], x[1]), x[2]))
 
-    # Compute the squared error for each matching entry
-    squaredErrorsRDD = (predictedReformattedRDD
-                        .join(actualReformattedRDD)
-                        .map(lambda x: (x[0], (math.pow((x[1][0]-x[1][1]), 2)))))
+#     # Compute the squared error for each matching entry
+#     squaredErrorsRDD = (predictedReformattedRDD
+#                         .join(actualReformattedRDD)
+#                         .map(lambda x: (x[0], (math.pow((x[1][0]-x[1][1]), 2)))))
 
-    totalError = sum(squaredErrorsRDD.values().take(squaredErrorsRDD.count()))
+#     totalError = sum(squaredErrorsRDD.values().take(squaredErrorsRDD.count()))
 
-    # Count the number of entries for which you computed the total squared error
-    numRatings = squaredErrorsRDD.count()
+#     # Count the number of entries for which you computed the total squared error
+#     numRatings = squaredErrorsRDD.count()
 
-    # Compute the RSME
-    return math.sqrt(totalError / numRatings)
+#     # Compute the RSME
+#     return math.sqrt(totalError / numRatings)
 
 header = raw_ratings.first()
 ratingsRDD = raw_ratings.filter(lambda line: line != header).map(get_ratings_tuple).cache()
 
-header = raw_movies.first()
-moviesRDD = raw_movies.filter(lambda line: line != header).map(get_movie_tuple).cache()
-movieIDsWithRatingsRDD = (ratingsRDD
-                          .map(lambda x: (x[1], x[2]))
-                          .groupByKey())
-movieIDsWithAvgRatingsRDD = movieIDsWithRatingsRDD.map(get_counts_and_averages)
-movieNameWithAvgRatingsRDD = (moviesRDD
-                              .join(movieIDsWithAvgRatingsRDD)
-                              .map(lambda x: (x[1][1][1], x[1][0], x[1][1][0])))
+# header = raw_movies.first()
+# moviesRDD = raw_movies.filter(lambda line: line != header).map(get_movie_tuple).cache()
+# movieIDsWithRatingsRDD = (ratingsRDD
+#                           .map(lambda x: (x[1], x[2]))
+#                           .groupByKey())
+# movieIDsWithAvgRatingsRDD = movieIDsWithRatingsRDD.map(get_counts_and_averages)
+# movieNameWithAvgRatingsRDD = (moviesRDD
+#                               .join(movieIDsWithAvgRatingsRDD)
+#                               .map(lambda x: (x[1][1][1], x[1][0], x[1][1][0])))
 # print movieNameWithAvgRatingsRDD.filter(lambda x: x[2] > 150).takeOrdered(3, lambda x: -x[0])
 
 # trainingRDD, validationRDD, testRDD = ratingsRDD.randomSplit([6, 2, 2], seed=0L)
@@ -231,7 +233,7 @@ def cosine_similarity(user1, user2):
 def similarities_for_user(user_id, crossRDD):
     """
     Takes in a user_id and and RDD of crossed user_ids with that user
-    Returns an array of the top 10 users who are most similar to a 
+    Returns an array of the top users who are most similar to a user.
     """
     similarities = (crossRDD
                     .filter(lambda x: x[0][0] == user_id)
@@ -242,7 +244,7 @@ def similarities_for_user(user_id, crossRDD):
 def similarities_for_vector(user_id, user_data_rdd, user_ids_with_ratings_rdd):
     """
     Takes in a tuple of form (user_id, sparse_vector_of_ratings) and combines it with existing
-    rating rdd. and returns cosine similarities for that user with existing users
+    rating rdd. Returns cosine similarities for that user with existing users
     sorted higheset to lowest
     """
     cross_rdd = user_data_rdd.cartesian(user_ids_with_ratings_rdd)
@@ -267,13 +269,15 @@ def create_most_similar_for_user_rdd(user_ratings, user_ids_with_ratings_rdd, mo
     return similarities_for_vector(0, create_user_with_sparse_ratings(0, user_ratings, movies_length), user_ids_with_ratings_rdd)
 
 def create_similar_users_and_similarity_rdd(mostSimilarForUserRDD):
-    return mostSimilarForUserRDD.sortBy(lambda x: -x[2]).map(lambda x: (x[1], x[2]))
+    return mostSimilarForUserRDD.map(lambda x: (x[1], x[2]))
+    # return mostSimilarForUserRDD.sortBy(lambda x: -x[2]).map(lambda x: (x[1], x[2]))
 
 def get_top_movies_for_user(user_ratings=fake_user_ratings, ratings_rdd=ratingsRDD):
     movies_length = ratings_rdd.map(lambda x: x[1]).max() + 1
     user_ids_with_ratings_rdd = (ratings_rdd
                                  .map(lambda (user_id, movie_id, rating): (user_id, [(movie_id, rating)]))
                                  .reduceByKey(lambda a, b: a + b)
+                                 .filter(lambda x: len(x[1]) > 25)
                                  .map(lambda x: (x[0], Vectors.sparse(movies_length, x[1]))))
     user_seen_movies_list = [x[0] for x in user_ratings]
     most_similar_for_user_rdd = create_most_similar_for_user_rdd(user_ratings, user_ids_with_ratings_rdd, movies_length)
@@ -281,8 +285,9 @@ def get_top_movies_for_user(user_ratings=fake_user_ratings, ratings_rdd=ratingsR
     top_movies_for_user = (user_ids_with_ratings_rdd
                            .join(similar_users_and_similarity_rdd)
                            .flatMap(lambda x: create_id_rating_tuples(x[1][1], x[1][0]))
-                           .reduceByKey(lambda a, b: max(a, b))
                            .filter(lambda x: x[0] not in user_seen_movies_list)
+                           # .takeOrdered(50, lambda x: -x[1]))
+                           .reduceByKey(lambda a, b: max(a, b))
                            .takeOrdered(50, lambda x: -x[1]))
     return top_movies_for_user
 
@@ -290,7 +295,9 @@ def get_top_movies_for_user(user_ratings=fake_user_ratings, ratings_rdd=ratingsR
 ratings = requests.get('http://localhost:5000/api/users/{}'.format(user_id)).json()['ratings']
 
 user_ratings = [(int(id), int(rating)) for id, rating in ratings]
+print get_top_movies_for_user(user_ratings)
 recommendations = {key: value for key, value in get_top_movies_for_user(user_ratings=user_ratings)}
+print recommendations
 r = requests.post('http://localhost:5000/api/users/{}'.format(user_id), data=json.dumps({"recommendations" : recommendations}))
 
 

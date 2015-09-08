c655743288030b4c55ce9904dbece2699d174f40
Cam Linke <cam.linke@gmail.com>
2015-09-08 14:38:39 -0600

    added a really ugly upload script to get movie-lense data up to s3 bucket

---
+++ aws/s3.py
@@ -0,0 +1,26 @@
+import boto3
+import uuid
+import os
+
+AWS_ACCESS_ID = os.environ['AWS_ACCESS_ID']
+AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+
+class S3:
+    def __init__(self):
+        self.session = boto3.Session(aws_access_key_id=AWS_ACCESS_ID, aws_secret_access_key=AWS_SECRET_KEY)
+        self.s3 = self.session.resource('s3')
+
+    def upload_file(self):
+        #small
+        self.s3.Object('movie-recommender', 'ml_data/ml-latest-small/movies.csv').put(Body=open("{}/ml-latest/movies.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "rb"))
+        self.s3.Object('movie-recommender', 'ml_data/ml-latest-small/links.csv').put(Body=open("{}/ml-latest/links.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "rb"))
+        self.s3.Object('movie-recommender', 'ml_data/ml-latest-small/ratings.csv').put(Body=open("{}/ml-latest-small/ratings.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "rb"))
+
+        #all
+        self.s3.Object('movie-recommender', 'ml_data/ml-latest/movies.csv').put(Body=open("{}/ml-latest/movies.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "rb"))
+        self.s3.Object('movie-recommender', 'ml_data/ml-latest/links.csv').put(Body=open("{}/ml-latest/links.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "rb"))
+        self.s3.Object('movie-recommender', 'ml_data/ml-latest/ratings.csv').put(Body=open("{}/ml-latest/ratings.csv".format(os.environ["MOVIE_DATA_LOCATION"]), "rb"))
+
+s = S3()
+s.upload_file()
+print s

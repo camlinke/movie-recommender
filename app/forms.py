7bc807aa26a0014507c4bf86395513d846f1eafd
Cam Linke <cam.linke@gmail.com>
2015-09-25 21:08:46 -0600

    pep8 forms.py

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,11 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
+
 class SignUpForm(Form):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
+
 
 class LoginForm(Form):
     email = StringField('Email', validators=[DataRequired()])
